cc9102e31d975560505dc0251bd13d046f67ffff
Matthew Trabold <mct4240@gmail.com>
2019-04-24 20:50:35 -0500

    Database Connection Added
    Added the database connection in server.js

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Public static folder
+app.use(express.static("public"));
+
+// Connect to Mongo DB *CHANGE NAME IN LOCAL IF NECESSARY*
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/cheerioScraper";
+mongoose.connect(MONGODB_URI);
