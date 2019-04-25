d64e3a095ddb444bf0306909f9d73981f68736f4
Matthew Trabold <mct4240@gmail.com>
2019-04-25 16:57:37 -0500

    Update server.js
    Routes for NY Times scraping added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+var db = require("./models");
+
 var PORT = 3000;
 
 // Initialize Express
@@ -29,11 +31,28 @@ app.get("/", function(req, res) {
 // A GET route for scraping hackerNews website
 app.get("/scrape", function(req, res) {
   // Grab the body of the html
-  axios.get("https://www.nytimes.com/").then(function(response) {
+  axios.get("https://www.nytimes.com/section/technology").then(function(response) {
     // Load the body into cheerio
     var $ = cheerio.load(response.data);
-    // Grab all articles with title class
-    var title = $(element).chldren("a").text();
-    var link = $(element).children("a").attr("href");
-  })
-})
+
+    // Grab each article with a class of css-ye6x8s
+    $(".css-ye6x8s").each(function(i, element) {
+      // Empty object for the results to be stored in
+      var result = {};
+
+      // Grab all articles with title class
+      result.headline = $(element).children(".css-1dq8tca e1xfvim30").text();
+      result.summary = $(element).children(".css-1echdzn e1xfvim31").text();
+      result.link = $(element).children("a").attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
