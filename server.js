c601e1fdbcad38e7e0617556861dd805eac531b1
Matthew Trabold <mct4240@gmail.com>
2019-04-24 22:36:04 -0500

    Setting up the Get Route
    In the process of setting up the routes for the website to push and pull data from the database

--- server.js
+++ server.js
@@ -18,3 +18,15 @@ app.use(express.static("public"));
 // Connect to Mongo DB *CHANGE NAME IN LOCAL IF NECESSARY*
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/cheerioScraper";
 mongoose.connect(MONGODB_URI);
+
+// A GET route for scraping hackerNews website
+app.get("/scrape", function(req, res) {
+  // Grab the body of the html
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    // Load the body into cheerio
+    var $ = cheerio.load(response.data);
+    // Grab all articles with title class
+    var title = $(element).chldren("a").text();
+    var link = $(element).children("a").attr("href");
+  })
+})
